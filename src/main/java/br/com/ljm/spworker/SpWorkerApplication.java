dc5bd51888d35fc15628c27c49edecc043e12d01
Leandro <leandro@Leandros-MacBook-Pro.local>
2022-06-04 23:38:42 -0300

    Project sp-worker

---
+++ src/main/java/br/com/ljm/spworker/SpWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ljm.spworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWorkerApplication.class, args);
+	}
+
+}
